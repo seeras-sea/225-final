95b0373d059c3a1ce94d057b3b254d50c371afca
seeras-sea <colli369@miamioh.edu>
2025-05-17 16:21:17 -0400

    Update run-tests-alt.sh

--- run-tests-alt.sh
+++ run-tests-alt.sh
@@ -7,4 +7,5 @@ echo "Flask service IP: $FLASK_IP"
 
 # Run the tests with a custom command that installs requests first
 echo "Running tests against Flask service at http://$FLASK_IP:5000"
-docker run --network=host -e FLASK_URL=http://$FLASK_IP:5000 qa-tests /bin/bash -c "pip install requests && python -m pytest test_html_elements.py -v"
+docker run --network=host -e FLASK_URL=http://10.43.39.124:5000 qa-tests /bin/bash -c "pip install requests && python -m pytest test_html_elements.py -v"
+
