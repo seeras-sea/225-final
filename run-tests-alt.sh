9868ce5827cc7674f7c0fa06d1b79c9b62a571be
seeras-sea <colli369@miamioh.edu>
2025-05-17 16:15:34 -0400

    Create run-tests-alt.sh

---
+++ run-tests-alt.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Get the Flask service IP
+FLASK_IP=$(kubectl get service flask-dev-service -o jsonpath='{.spec.clusterIP}')
+
+echo "Flask service IP: $FLASK_IP"
+
+# Run the tests with a custom command that installs requests first
+echo "Running tests against Flask service at http://$FLASK_IP:5000"
+docker run --network=host -e FLASK_URL=http://$FLASK_IP:5000 qa-tests /bin/bash -c "pip install requests && python -m pytest test_html_elements.py -v"
